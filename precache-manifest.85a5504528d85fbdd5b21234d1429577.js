343f31df1c329f498b9bab5a3dbff838ec18c97b
David Gilbert <173236+digitalDG@users.noreply.github.com>
2021-03-27 22:25:41 -0400

    Updates

---
+++ precache-manifest.85a5504528d85fbdd5b21234d1429577.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b7bf60544ecb84bcefc2c92ff515a2",
+    "url": "/fed-challenge-starter/index.html"
+  },
+  {
+    "revision": "953ba73d052746d5b803",
+    "url": "/fed-challenge-starter/static/css/main.ff411040.chunk.css"
+  },
+  {
+    "revision": "7f886cdfa6350a15db1d",
+    "url": "/fed-challenge-starter/static/js/2.a3412156.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/fed-challenge-starter/static/js/2.a3412156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953ba73d052746d5b803",
+    "url": "/fed-challenge-starter/static/js/main.b761558e.chunk.js"
+  },
+  {
+    "revision": "ddf4e5f1c600b5f0db56",
+    "url": "/fed-challenge-starter/static/js/runtime-main.3972a04a.js"
+  },
+  {
+    "revision": "1542c1188204879edee18cf6e115d934",
+    "url": "/fed-challenge-starter/static/media/20-minutes-to-toned-thumb.1542c118.jpg"
+  },
+  {
+    "revision": "003901761c4b2f91b5c1b144747d4e83",
+    "url": "/fed-challenge-starter/static/media/charles-race-thumb.00390176.jpg"
+  },
+  {
+    "revision": "578dacbc0d22c3d6ff585347dadb0e73",
+    "url": "/fed-challenge-starter/static/media/full-body-hiit-thumb.578dacbc.jpg"
+  },
+  {
+    "revision": "a55f946a95a88b198f5fb3f1173a95f6",
+    "url": "/fed-challenge-starter/static/media/kafue-river-thumb.a55f946a.jpg"
+  },
+  {
+    "revision": "e0a7c9061a0e72bed419a8b9eaf2b286",
+    "url": "/fed-challenge-starter/static/media/lake-inniscarra-thumb.e0a7c906.jpg"
+  },
+  {
+    "revision": "3e82c42342a27af7cce1e89930288333",
+    "url": "/fed-challenge-starter/static/media/performance-series-thumb.3e82c423.jpg"
+  },
+  {
+    "revision": "04edd5427547b03287f6d75cfabea837",
+    "url": "/fed-challenge-starter/static/media/shred-and-burn-thumb.04edd542.jpg"
+  },
+  {
+    "revision": "7cae9ed133157efe82011594ed819bcc",
+    "url": "/fed-challenge-starter/static/media/slow-pulls-thumb.7cae9ed1.jpg"
+  }
+]);
